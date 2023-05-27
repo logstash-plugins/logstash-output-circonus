43452b63e9af2da196507226772621e38cfe31d1
Mashhur <mashhur.sattorov@elastic.co>
2023-05-27 15:16:52 -0700

    Add unit test case.

--- spec/outputs/circonus_spec.rb
+++ spec/outputs/circonus_spec.rb
@@ -1,1 +1,19 @@
 require "logstash/devutils/rspec/spec_helper"
+require 'logstash/outputs/circonus'
+
+describe LogStash::Outputs::Circonus do
+
+  let(:plugin) { described_class.new(config) }
+
+  describe "debugging `api_token`" do
+    let(:config) {{ "app_name" => "my-app-name", "api_token" => "$ecre&-key" }}
+
+    it "should not show origin value" do
+      expect(plugin.logger).to receive(:debug).with('<password>')
+
+      plugin.register
+      plugin.logger.send(:debug, plugin.api_token.to_s)
+    end
+  end
+
+end
